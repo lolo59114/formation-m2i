d2d878bcdb61134f570b2edfbdafb27c467a913d
VI Loic <vaninghelandtloic@gmail.com>
2024-08-22 14:23:30 +0200

    Javascript base exercice24 bonus

--- Javascript_base/exercice24/script.js
+++ Javascript_base/exercice24/script.js
@@ -10,6 +10,31 @@ function listeEtudiants(etudiants) {
     }
 }
 
+function affichageListeEtudiants(etudiants) {
+    document.title = "Liste des étudiants";
+    const main = document.querySelector("main");
+    const listeEtudiants = document.createElement("ul");
+    for(const etudiant of etudiants) {
+        const ligneEtudiant = document.createElement("li");
+        const listeMatieres = document.createElement("ul");
+        const ligneMoyenne = document.createElement("li");
+        ligneEtudiant.innerText = `Etudiant : ${etudiant.nom} ${etudiant.prenom}`;
+        listeEtudiants.appendChild(ligneEtudiant);
+        const {matieres} = etudiant;
+        for(const matiere in matieres) {
+            const ligneMatiere = document.createElement("li");
+            ligneMatiere.textContent = `${matiere} : ${matieres[matiere]}/20`;
+            listeMatieres.appendChild(ligneMatiere);
+        }
+        let moyenneGenerale = Object.values(matieres).reduce((total, note) => total + note) / Object.keys(matieres).length;
+        ligneMoyenne.textContent = "La moyenne est de : " + moyenneGenerale.toFixed(1) + "/20";
+        listeMatieres.appendChild(ligneMoyenne);
+        listeEtudiants.appendChild(listeMatieres);
+        
+    }
+    main.appendChild(listeEtudiants);
+}
+
 let etudiants = [ 
     { 
         prenom: "José", 
@@ -33,4 +58,5 @@ let etudiants = [
     } 
 ];
 
-listeEtudiants(etudiants);
+affichageListeEtudiants(etudiants);
+// listeEtudiants(etudiants);
