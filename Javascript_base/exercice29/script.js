9fc94178de44dbcdb3e42dbe0d7063826908837b
VI Loic <vaninghelandtloic@gmail.com>
2024-08-26 09:44:18 +0200

    Javascript base Exercice 29

--- Javascript_base/exercice29/script.js
+++ Javascript_base/exercice29/script.js
@@ -1,10 +1,24 @@
+import Contact from "./class/contact.js";
+
+let listContacts = [];
+const tableBody = document.querySelector("#table-contacts tbody");
+    
+function addContact(contact) {
+    listContacts.push(contact);
+    const tableTr = document.createElement("tr");
+    for(let property in contact) {
+        const tableTd = document.createElement("td");
+        tableTd.textContent = contact[property];
+        tableTr.appendChild(tableTd);
+    }
+    tableBody.appendChild(tableTr);
+}
 
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const tableBody = document.querySelector("#table-contacts tbody");
-    const tableTr = document.createElement("tr");
-    
-    console.log(tableBody);
+    let myFormData = new FormData(e.target);
+    let contact = new Contact(myFormData.get("title"), myFormData.get("firstname"), myFormData.get("lastname"), myFormData.get("birthdate"), myFormData.get("phone-number"), myFormData.get("email"));
+    console.log(contact);
+    addContact(contact);
+    e.target.reset();
 });
-
-
