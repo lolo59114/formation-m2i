ecc05df36f0e38b2bbc92295caaeecc19cd0171e
VI Loic <vaninghelandtloic@gmail.com>
2024-08-27 11:55:22 +0200

    Javascript base bonus

--- Javascript_base/exercicePOOBonus/js/script.js
+++ Javascript_base/exercicePOOBonus/js/script.js
@@ -1,9 +1,10 @@
 import Ticket from "./class/ticket.js";
 
-let listTickets = [];
+let listTickets = localStorage;
 const messageInfo = document.getElementById("message-info");
 const messageInfoInitClass = [...messageInfo.classList].join(" ");
 
+console.log(listTickets);
 function checkImmatriculation(immat) {
     return immat.length == 10;
 }
@@ -41,26 +42,28 @@ function displayMessage(message, type="INFO", timer=5000) {
 }
     
 function addTicket(ticket) {
-    if(listTickets.find((t) => t.immatriculation == ticket.immatriculation)) {
+    if(listTickets.getItem(ticket.immatriculation)) {
         displayMessage(`Le véhicule ${ticket.immatriculation} possède déjà un ticket depuis ${ticket.arrivalDate}`, "WARNING");
     } else {
-        listTickets.push(ticket);
+        listTickets.setItem(ticket.immatriculation, ticket.arrivalDate.getTime());
         displayMessage(`Veuillez prendre votre ticket pour le véhicule ${ticket.immatriculation}.`, "INFO");
     }
 }
 
-function payTicket(immat) {
-    let ticket = listTickets.find((t) => t.immatriculation == immat);
-    if(ticket == null) {
-        displayMessage(`Le véhicule ${immat} n'existe pas!`, "ERROR");
-    } else {
-        let price = getPriceFromTicket(ticket);
-        displayMessage(`Le prix à payer pour le véhicule ${immat} est de ${price} €`, "WARNING");
-        // same as : listTickets = listTickets.filter(ticket => ticket.immatriculation !== immat);
-        listTickets.splice(listTickets.indexOf(ticket), 1);
-    }
+function removeTicket(ticket) {
+    listTickets.removeItem(ticket.immatriculation);
 }
 
+function payTicket(immat) {
+    if(listTickets.getItem(immat) == null) {
+        displayMessage(`Le véhicule ${immat} n'existe pas!`, "ERROR");
+    } else {
+        let ticket = new Ticket(immat, listTickets.getItem(immat));
+        let price = getPriceFromTicket(ticket);
+        displayMessage(`Le prix à payer pour le véhicule ${immat} est de ${price} €`, "WARNING");
+        removeTicket(ticket);
+    }
+}
 
 document.getElementById("btn-get-ticket").addEventListener("click", () => {
     let immat = document.getElementById("immatriculation").value;
