73ecee80a4159d0ffac10f442cd60dd20b7efcc0
VI Loic <vaninghelandtloic@gmail.com>
2024-09-05 10:59:19 +0200

    Angular ajout display list pokemon

--- Angular/exercice6/src/app/pages/pokemon/pokemon.component.ts
+++ Angular/exercice6/src/app/pages/pokemon/pokemon.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-
+import {UpperCasePipe} from "@angular/common";
+export type Pokemon = Partial<{
+  name: string | null | undefined,
+  description: string | null | undefined,
+  attacks:
+    Partial<{
+      name: string | null | undefined,
+      description: string | null | undefined,
+      damage: number | null | undefined,
+    }>[] | undefined,
+  types: (string|null)[] | null | undefined,
+  area: Partial<{
+    name: string | null | undefined,
+    region: string | null | undefined,
+  }> | undefined,
+}>
 @Component({
   selector: 'app-pokemon',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    UpperCasePipe
   ],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.css'
 })
 export class PokemonComponent {
+  pokemons: Pokemon[] = [];
+
   pokemonForm = new FormGroup({
     name: new FormControl("", [Validators.required]),
     description: new FormControl("", [Validators.required]),
@@ -50,27 +68,31 @@ export class PokemonComponent {
     return this.pokemonForm.controls.area;
   }
 
-
   savePokemon(): void {
     if(this.pokemonForm.valid) {
-      console.log(this.pokemonForm.value);
+      let pokemon: Pokemon = {...this.pokemonForm.value}
+      this.pokemons.push(pokemon);
       this.pokemonForm.reset();
     }
   }
 
-  addType() {
+  addType(): void {
     this.types.push(new FormControl(""));
   }
 
-  removeType() {
+  removeType(): void {
     this.types.removeAt(this.types.length - 1);
   }
 
-  addAttack() {
+  addAttack(): void {
     this.attacks.push(new FormGroup({
       name: new FormControl("", [Validators.required]),
       description: new FormControl("", [Validators.required]),
       damage: new FormControl(0, [Validators.min(0)]),
     }));
+  }
+
+  removeAttack(): void {
+    this.attacks.removeAt(this.attacks.length - 1);
   }
 }
